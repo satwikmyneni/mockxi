db5205dfe7e1388c573b680a987f3996d0ca3830
Satwik Myneni <mynenisatvik@gmail.com>
2026-03-23 14:06:23 +0530

    debug: show full AI error

--- netlify/functions/ai-proxy.js
+++ netlify/functions/ai-proxy.js
@@ -1,4 +1,4 @@
-exports.handler = async function(event) {
+exports.handler = async function (event) {
   if (event.httpMethod === 'OPTIONS') {
     return { statusCode: 200, headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Content-Type', 'Access-Control-Allow-Methods': 'POST, OPTIONS' }, body: '' };
   }
